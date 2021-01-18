dacc397e6c76b3aa8b09330c65157e89f9e88393
shywj05 <shywj05@gmail.com>
2021-01-18 19:11:20 +0900

    .

--- src/f_OOP2/Interface_01.java
+++ src/f_OOP2/Interface_01.java
@@ -5,7 +5,7 @@ public class Interface_01 {
 
 		Fight f = new Fight();
 		
-		System.out.println(f instanceof Fight);
+		System.out.println(f instanceof Fight);//instanceof는 형변환 가능한지 확인해 주는 녀석이다.
 		System.out.println(f instanceof Unit);
 		System.out.println(f instanceof Object);
 		
@@ -15,36 +15,38 @@ public class Interface_01 {
 		System.out.println(f instanceof Movable);
 		Movable mv = new Fight();
 		
+		System.out.println(f instanceof Attackable); //형변환 가능 
+		Attackable at = new Fight();
 		
 		
 			
 		}
 	}
 
-class Fight extends Unit implements Fightable{
+class Fight extends Unit implements Fightable{//인터페이스 구현 시 implement를 쓴다.
 
 	@Override
-	public void attack(Unit u) {
+	public void attack(Unit u) {//Fightable의 implement니까 
 		
 	}
 
 	@Override
-	public void move(int x, int y) {
-
+	public void move(int x, int y) {//인터페이스 Fightable을 구현하고  Unit의 클래스를 상속받는 Fight클래스니까 오버라이드해서 사용
+									// 
 	}
 	
 }
 
-interface Fightable extends Movable, Attackable{
+interface Fightable extends Movable, Attackable{//인터페이스는 다중 상속이 가능하다. 어차피 몸통에 아무것도 없으니까  상관없지
 	
 }
 
 interface Movable{
-	void move(int x, int y);//구현부가 없어야 한다
+	void move(int x, int y);//인터페이스 구현부는 추상메서드로 만들어져야 하기 때문에 구현부가 없어야 한다.
 }
 
 interface Attackable{
-	void attack(Unit u);
+	void attack(Unit u);//접근 제어자가 생략되어 있지? public abstract
 }
 
 
