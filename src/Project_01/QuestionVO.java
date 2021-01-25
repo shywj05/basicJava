fdd9f7e10b5613153d32835fe77b21377efe6416
shywj05 <76418801+shywj05@users.noreply.github.com>
2021-01-26 01:45:08 +0900

    .

---
+++ src/Project_01/QuestionVO.java
@@ -0,0 +1,52 @@
+package Project_01;
+
+import java.util.Date;
+
+public class QuestionVO {
+	private int num;
+	private String date;
+	private String title;
+	private String content;
+	private String answer;
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
